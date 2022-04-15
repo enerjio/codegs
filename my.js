07ba8b164f4ed6aecf5ab965dfb21a3fbd53553f
enerjio <75221271+enerjio@users.noreply.github.com>
2022-04-15 16:30:24 +0300

    Create my.js

---
+++ my.js
@@ -0,0 +1,41 @@
+
+function testGS(){
+
+    const url ="https://script.google.com/macros/s/AKfycbxyvTwQg2lCBn2pHWPhItvfyUh7gEYgncy5MJefDZ3MGWMCBkjQ/exec";
+
+
+    fetch(url)
+    .then(d => d.json())
+    .then(d=>{
+          document.getElementById("app").textContent = d[0].status;
+    });
+
+}
+
+
+function addGS(){
+
+    const url ="https://script.google.com/macros/s/AKfycbxyvTwQg2lCBn2pHWPhItvfyUh7gEYgncy5MJefDZ3MGWMCBkjQ/exec";
+
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'omit', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({first:"Olivia", last: "ulutaş",phone: "12312312312"}) // body data type must match "Content-Type" header
+      });
+      
+
+}
+
+document.getElementById("btn").addEventListener("click",addGS);
+
+document.getElementById("btn2").addEventListener("click",testGS);
+
